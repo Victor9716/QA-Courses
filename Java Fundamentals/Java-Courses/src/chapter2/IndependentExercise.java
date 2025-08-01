8361881c6ada8f6de61157ccf2350314ac5e62c8
Victor9716 <163063954+Victor9716@users.noreply.github.com>
2025-08-01 12:07:04 -0500

    Add files via upload

---
+++ Java Fundamentals/Java-Courses/src/chapter2/IndependentExercise.java
@@ -0,0 +1,26 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class IndependentExercise {
+
+    //1. Get the season of the year
+    public static void main(String[] args) {
+        System.out.println("Enter the season of the year:");
+        Scanner scanner = new Scanner(System.in);
+        String season = scanner.next();
+
+        //2. get an adjective
+        System.out.println("Describe the season with an adjective:");
+        season = scanner.next();
+        scanner.close();
+
+
+        //3.  get a whole number
+        System.out.println("minimum number of:");
+
+        //4. output
+    }
+
+
+}
